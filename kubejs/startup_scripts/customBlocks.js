4b0aa57e25b362fd4b6bbcb4f0700f23d321d0a2
DerCoderJo <108522996+DerCoderJo@users.noreply.github.com>
2024-11-09 12:31:32 +0100

    Added Rusty Iron Blocks

---
+++ kubejs/startup_scripts/customBlocks.js
@@ -0,0 +1,5 @@
+onEvent("block.registry", (event) => {
+    event.create("create_attack_6:rusty_iron_block_light").material("metal")
+    event.create("create_attack_6:rusty_iron_block_middle").material("metal")
+    event.create("create_attack_6:rusty_iron_block_strong").material("metal")
+})
