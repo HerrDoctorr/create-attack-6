b23a0f015bc9fdec42d5aac6eb2af65f0b115603
DerCoderJo <108522996+DerCoderJo@users.noreply.github.com>
2024-11-12 21:18:29 +0100

    Added Money

--- kubejs/startup_scripts/customItems.js
+++ kubejs/startup_scripts/customItems.js
@@ -1,4 +1,5 @@
 onEvent("item.registry", (event) => {
+    // Aircraft Items
     event.create("create_attack_6:incomplete_engine").texture("create_attack_6:item/incomplete_engine")
     event.create("create_attack_6:incomplete_boiler").texture("create_attack_6:item/incomplete_boiler")
     event.create("create_attack_6:incomplete_eco_engine").texture("create_attack_6:item/incomplete_eco_engine")
@@ -7,7 +8,19 @@ onEvent("item.registry", (event) => {
     event.create("create_attack_6:incomplete_gyroscope").texture("create_attack_6:item/incomplete_gyroscope")
     event.create("create_attack_6:wheel").texture("create_attack_6:item/wheel")
 
+    // Rusty Iron
     event.create("create_attack_6:rusty_iron_light").texture("create_attack_6:item/rusty_iron_light")
     event.create("create_attack_6:rusty_iron_middle").texture("create_attack_6:item/rusty_iron_middle")
     event.create("create_attack_6:rusty_iron_strong").texture("create_attack_6:item/rusty_iron_strong")
+
+    // Currency Items
+    event.create("create_attack_6:one_tinker_token").texture("create_attack_6:item/tinker_tokens/one_tinker_token")
+    event.create("create_attack_6:two_tinker_tokens").texture("create_attack_6:item/tinker_tokens/two_tinker_tokens")
+    event.create("create_attack_6:five_tinker_tokens").texture("create_attack_6:item/tinker_tokens/five_tinker_tokens")
+    event.create("create_attack_6:ten_tinker_tokens").texture("create_attack_6:item/tinker_tokens/ten_tinker_tokens")
+    event.create("create_attack_6:twenty_tinker_tokens").texture("create_attack_6:item/tinker_tokens/twenty_tinker_tokens")
+    event.create("create_attack_6:fifty_tinker_tokens").texture("create_attack_6:item/tinker_tokens/fifty_tinker_tokens")
+    event.create("create_attack_6:one_hundred_tinker_tokens").texture("create_attack_6:item/tinker_tokens/one_hundred_tinker_tokens")
+    event.create("create_attack_6:two_hundred_tinker_tokens").texture("create_attack_6:item/tinker_tokens/two_hundred_tinker_tokens")
+    event.create("create_attack_6:five_hundred_tinker_tokens").texture("create_attack_6:item/tinker_tokens/five_hundred_tinker_tokens")
 })
